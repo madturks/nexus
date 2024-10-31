2d8b686966553a2897c47e35c30b4a1ef581d54d
mkg <hello@mkg.dev>
2024-10-31 16:40:39 +0100

    [msquic_client] add initial set of unit tests
    - implement a mock msquic_application for testing
    
    Signed-off-by: mkg <hello@mkg.dev>

--- madturks/nexus/test/unit/ut_msquic_application.cpp
+++ madturks/nexus/test/unit/ut_msquic_application.cpp
@@ -12,87 +12,9 @@
 #include <gtest/gtest.h>
 #include <msquic.h>
 
-#include <memory>
+#include "mock_msquic_api.hpp"
 
 namespace mad::nexus {
-
-struct mock_msquic_api : public QUIC_API_TABLE {
-    struct default_mock_functions {
-        static_mock<QUIC_SET_CONTEXT_FN> SetContext;
-        static_mock<QUIC_GET_CONTEXT_FN> GetContext;
-        static_mock<QUIC_SET_CALLBACK_HANDLER_FN> SetCallbackHandler;
-        static_mock<QUIC_SET_PARAM_FN> SetParam;
-        static_mock<QUIC_GET_PARAM_FN> GetParam;
-        static_mock<QUIC_REGISTRATION_OPEN_FN> RegistrationOpen;
-        static_mock<QUIC_REGISTRATION_CLOSE_FN> RegistrationClose;
-        static_mock<QUIC_REGISTRATION_SHUTDOWN_FN> RegistrationShutdown;
-        static_mock<QUIC_CONFIGURATION_OPEN_FN> ConfigurationOpen;
-        static_mock<QUIC_CONFIGURATION_CLOSE_FN> ConfigurationClose;
-        static_mock<QUIC_CONFIGURATION_LOAD_CREDENTIAL_FN>
-            ConfigurationLoadCredential;
-        static_mock<QUIC_LISTENER_OPEN_FN> ListenerOpen;
-        static_mock<QUIC_LISTENER_CLOSE_FN> ListenerClose;
-        static_mock<QUIC_LISTENER_START_FN> ListenerStart;
-        static_mock<QUIC_LISTENER_STOP_FN> ListenerStop;
-        static_mock<QUIC_CONNECTION_OPEN_FN> ConnectionOpen;
-        static_mock<QUIC_CONNECTION_CLOSE_FN> ConnectionClose;
-        static_mock<QUIC_CONNECTION_SHUTDOWN_FN> ConnectionShutdown;
-        static_mock<QUIC_CONNECTION_START_FN> ConnectionStart;
-        static_mock<QUIC_CONNECTION_SET_CONFIGURATION_FN>
-            ConnectionSetConfiguration;
-        static_mock<QUIC_CONNECTION_SEND_RESUMPTION_FN>
-            ConnectionSendResumptionTicket;
-        static_mock<QUIC_STREAM_OPEN_FN> StreamOpen;
-        static_mock<QUIC_STREAM_CLOSE_FN> StreamClose;
-        static_mock<QUIC_STREAM_START_FN> StreamStart;
-        static_mock<QUIC_STREAM_SHUTDOWN_FN> StreamShutdown;
-        static_mock<QUIC_STREAM_SEND_FN> StreamSend;
-        static_mock<QUIC_STREAM_RECEIVE_COMPLETE_FN> StreamReceiveComplete;
-        static_mock<QUIC_STREAM_RECEIVE_SET_ENABLED_FN> StreamReceiveSetEnabled;
-        static_mock<QUIC_DATAGRAM_SEND_FN> DatagramSend;
-        static_mock<QUIC_CONNECTION_COMP_RESUMPTION_FN>
-            ConnectionResumptionTicketValidationComplete;
-        static_mock<QUIC_CONNECTION_COMP_CERT_FN>
-            ConnectionCertificateValidationComplete;
-    } default_mock;
-
-    mock_msquic_api() {
-        SetContext = default_mock.SetContext;
-        GetContext = default_mock.GetContext;
-        SetCallbackHandler = default_mock.SetCallbackHandler;
-        SetParam = default_mock.SetParam;
-        GetParam = default_mock.GetParam;
-        RegistrationOpen = default_mock.RegistrationOpen;
-        RegistrationClose = default_mock.RegistrationClose;
-        RegistrationShutdown = default_mock.RegistrationShutdown;
-        ConfigurationOpen = default_mock.ConfigurationOpen;
-        ConfigurationClose = default_mock.ConfigurationClose;
-        ConfigurationLoadCredential = default_mock.ConfigurationLoadCredential;
-        ListenerOpen = default_mock.ListenerOpen;
-        ListenerClose = default_mock.ListenerClose;
-        ListenerStart = default_mock.ListenerStart;
-        ListenerStop = default_mock.ListenerStop;
-        ConnectionOpen = default_mock.ConnectionOpen;
-        ConnectionClose = default_mock.ConnectionClose;
-        ConnectionShutdown = default_mock.ConnectionShutdown;
-        ConnectionStart = default_mock.ConnectionStart;
-        ConnectionSetConfiguration = default_mock.ConnectionSetConfiguration;
-        ConnectionSendResumptionTicket =
-            default_mock.ConnectionSendResumptionTicket;
-        StreamOpen = default_mock.StreamOpen;
-        StreamClose = default_mock.StreamClose;
-        StreamStart = default_mock.StreamStart;
-        StreamShutdown = default_mock.StreamShutdown;
-        StreamSend = default_mock.StreamSend;
-        StreamReceiveComplete = default_mock.StreamReceiveComplete;
-        StreamReceiveSetEnabled = default_mock.StreamReceiveSetEnabled;
-        DatagramSend = default_mock.DatagramSend;
-        ConnectionResumptionTicketValidationComplete =
-            default_mock.ConnectionResumptionTicketValidationComplete;
-        ConnectionCertificateValidationComplete =
-            default_mock.ConnectionCertificateValidationComplete;
-    }
-};
 
 struct tf_msquic_application : public ::testing::Test {
 
